a65c2785111d565f5dbc6355045d243411f63f59
Ingrid04 <ingridfmelgar@gmail.com>
2021-03-05 14:44:48 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home.js'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+
+  render() {
+    
+    return (
+
+      <div>
+        <h1>Login</h1>
+        <h1>Sign Up</h1>
+        <img className="homepicture" src="images/doctorpic.jpg" alt="doctorpichome" /> 
     </div>
-  );
+    )
+    
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => (
+
+    <div>
+        <h1>Login</h1>
+        <h1>Sign Up</h1>
+    </div>
+
+
+);
+
+export default Home;
