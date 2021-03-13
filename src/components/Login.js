43f17587a86138e9c3ec34b45ee03e8fb04f669c
Ingrid04 <ingridfmelgar@gmail.com>
2021-03-12 23:42:54 -0500

    login page updates

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,29 @@ import React from 'react';
 const Login = () => {
 
         return(
-            <div>
-                Login page
-            </div>
+
+            <form>
+                <div class="container">
+                    <label > Username </label>
+                    <input type="text" placeholder="Enter Username" name="username" required />
+
+                    <label > Password </label>
+                    <input type="password" placeholder="Enter Password" name="password" required />
+
+                    <button type="submit"> Login </button>
+
+                    <label> 
+                        <input type="checkbox" checked="checked" name="remember" /> Remember me
+                    </label>
+                </div>
+
+                <div class="container">
+                    <button type="button" class="cancelbtn">Cancel</button>
+                    <span class="psw">Forgot <a href="#">password?</a></span>
+                </div>
+
+            </form>
+            
         )
 
 }
